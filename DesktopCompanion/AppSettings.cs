542cfe284b57db70f6e68e4861443be276ebb078
Mats Gefvert <mats@gefvert.org>
2021-08-26 09:22:25 -0400

    Initial commit

---
+++ DesktopCompanion/AppSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DotNetCommons.Temporal;
+using Microsoft.Win32;
+
+namespace DesktopCompanion
+{
+    public class AppSettings
+    {
+        private readonly RegistryKey _regKeyDesktopCompanion;
+        private readonly RegistryKey _regKeyDesktopCompanionHolidays;
+
+        public AppSettings()
+        {
+            _regKeyDesktopCompanion = Registry.CurrentUser.OpenSubKey("Software\\Gefvert\\DesktopCompanion", true);
+            _regKeyDesktopCompanionHolidays = Registry.CurrentUser.OpenSubKey("Software\\Gefvert\\DesktopCompanion\\Holidays", false);
+        }
+
+        public int WallpaperOffset
+        {
+            get => (int)(_regKeyDesktopCompanion.GetValue("WallpaperOffset", 0) ?? 0);
+            set => _regKeyDesktopCompanion.SetValue("WallpaperOffset", value);
+        }
+
+        public IEnumerable<Holiday> Holidays =>
+            _regKeyDesktopCompanionHolidays.GetValueNames()
+                .Select(x => Holiday.Create((string)_regKeyDesktopCompanionHolidays.GetValue(x)));
+    }
+}
